ca852449a4d45fd87c5989f0c0caa32579fd3d65
Young-Murloc <78486158+Young-Murloc@users.noreply.github.com>
2021-02-06 18:31:11 +0900

    1D1Q

---
+++ 1일1문제/두개뽑아서더하기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+
+    for (int i = 0; i < numbers.size() - 1; i++) {
+        for (int j = i + 1; j < numbers.size(); j++) {
+            answer.push_back(numbers[i] + numbers[j]);
+        }
+    }
+
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(), answer.end()), answer.end());
+
+    for (int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << endl;
+    }
+
+    return answer;
+}
